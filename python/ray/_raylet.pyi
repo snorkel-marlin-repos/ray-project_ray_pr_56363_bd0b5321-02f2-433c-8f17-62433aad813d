ea385620697b873e4869d878d90f295582801092
iamjustinhsu <140442892+iamjustinhsu@users.noreply.github.com>
2025-09-08 17:02:20 -0700

    [data] Rename env var for enforce schemas (#56254)
    <!-- Thank you for your contribution! Please review
    https://github.com/ray-project/ray/blob/master/CONTRIBUTING.rst before
    opening a pull request. -->
    
    <!-- Please add a reviewer to the assignee section when you create a PR.
    If you don't have the access to it, we will shortly find a reviewer and
    assign them to your PR. -->
    
    ## Why are these changes needed?
    As titled
    <!-- Please give a short summary of the change and the problem this
    solves. -->
    
    ## Related issue number
    
    <!-- For example: "Closes #1234" -->
    
    ## Checks
    
    - [ ] I've signed off every commit(by using the -s flag, i.e., `git
    commit -s`) in this PR.
    - [ ] I've run `scripts/format.sh` to lint the changes in this PR.
    - [ ] I've included any doc changes needed for
    https://docs.ray.io/en/master/.
    - [ ] I've added any new APIs to the API Reference. For example, if I
    added a
    method in Tune, I've added it in `doc/source/tune/api/` under the
               corresponding `.rst` file.
    - [ ] I've made sure the tests are passing. Note that there might be a
    few flaky tests, see the recent failures at https://flakey-tests.ray.io/
    - Testing Strategy
       - [ ] Unit tests
       - [ ] Release tests
       - [ ] This PR is not tested :(
    
    ---------
    
    Signed-off-by: iamjustinhsu <jhsu@anyscale.com>

---
+++ python/ray/_raylet.pyi
@@ -0,0 +1,42 @@
+from ray.includes.object_ref import (
+    _set_future_helper,
+    ObjectRef
+)
+
+from ray.includes.unique_ids import (
+    check_id,
+    BaseID,
+    UniqueID,
+    TaskID,
+    NodeID,
+    JobID,
+    WorkerID,
+    ActorID,
+    FunctionID,
+    ActorClassID,
+    ClusterID,
+    ObjectID,
+    PlacementGroupID,
+)
+
+
+__all__ = [
+    # ray.includes.unique_ids
+    "ActorClassID",
+    "ActorID",
+    "BaseID",
+    "ClusterID",
+    "FunctionID",
+    "JobID",
+    "NodeID",
+    "ObjectID",
+    "PlacementGroupID",
+    "TaskID",
+    "UniqueID",
+    "WorkerID",
+    "check_id",
+
+    # ray.includes.object_ref
+    "_set_future_helper",
+    "ObjectRef",
+]
---
+++ python/ray/experimental/collective/__init__.py
@@ -0,0 +1,24 @@
+from ray.experimental.collective.operations import (
+    allgather,
+    allreduce,
+    reducescatter,
+)
+from ray.experimental.collective.collective import (
+    get_collective_groups,
+    create_collective_group,
+    destroy_collective_group,
+    destroy_all_collective_groups,
+)
+from ray.experimental.collective.util import get_tensor_transport_manager
+
+
+__all__ = [
+    "allgather",
+    "allreduce",
+    "reducescatter",
+    "get_collective_groups",
+    "create_collective_group",
+    "destroy_collective_group",
+    "destroy_all_collective_groups",
+    "get_tensor_transport_manager",
+]
